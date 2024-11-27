34d3a577aa2c3e4af4801a6082417c06993a5cfb
Alazar Aklilu <zar.aklilu@gmail.com>
2024-11-26 22:22:43 -0500

    initial updates

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Static files
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
+
+// unknown middleware error 
+app.use((err, req, res, next) => {
+    const defaultErr = {
+        log: 'Express error handler caught unknown middleware error',
+        status: 500,
+        message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}...`);
+});
