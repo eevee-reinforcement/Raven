b27f20cb12260053152fc3d9f423f807f7369d79
Micah Ziegler <micahjziegler@gmail.com>
2024-11-30 10:28:31 -0500

    Installed supabase-js. Created supabase client in App.jsx. Added basic functionality to get messages to App.jsx.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://eutmuhtelnmjxioxfbud.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1dG11aHRlbG5tanhpb3hmYnVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2NzMxMjMsImV4cCI6MjA0ODI0OTEyM30.CInfKoQipixQm4UJD8a_piZYOdd7iuaYEOUc5EV5u2o");
 
 const App = () => {
+  const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+      getMessages();
+    }, []);
+
+    async function getMessages() {
+      const { data } = await supabase.from("messages").select();
+      setMessages(data);
+    }
   return (
-    <div>Hello world</div>
+    <div>
+      <h1>Hello world</h1>
+      <ul>
+        {messages.map((message) => (
+          <li key={message.message_id}>{message.user_id}: {message.message_content}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
