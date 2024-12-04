2a7228222cee7232c48d320a866f27d0623b1530
Ryan Gilbert <ryanbgilbert@gmail.com>
2024-12-03 18:03:17 -0800

    Setup React Router

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Chatroom from '../src/components/Chatroom.jsx';
+import AuthPage from '../src/components/AuthPage.jsx';
+import EventSettings from '../src/components/EventSettings.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="chat" element={<Chatroom />} />
+        <Route path="signin" element={<AuthPage />} />
+        <Route path="event" element={<EventSettings />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
